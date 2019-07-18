ef22e45b93abc3b307b082269c4d7c38514f5b05
Markus Scheidgen <markus.scheidgen@gmail.com>
2019-07-18 13:53:30 +0200

    Moved to 3.7.

--- README.rst
+++ README.rst
@@ -1,15 +1,15 @@
-This is a backport of the zipfile module from Python 3.6, which contains some
-improvements.
+This is a backport of the zipfile module from Python 3.7, which contains some
+improvements. Cloned from the 3.6 backport of Thomas Kluyver (https://gitlab.com/takluyver/zipfile36).
 
 Installation::
 
-    pip install zipfile36
+    pip install zipfile37
 
 Suggested usage:
 
 .. code:: python
 
-    if sys.version_info >= (3, 6):
+    if sys.version_info >= (3, 7):
         import zipfile
     else:
-        import zipfile36 as zipfile
+        import zipfile37 as zipfile
--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 with open("README.rst", "r") as f:
     readme = f.read()
 
-setup(name='zipfile36',
+setup(name='zipfile37',
       version='0.1.3',
-      description='Read and write ZIP files - backport of the zipfile module from Python 3.6',
+      description='Read and write ZIP files - backport of the zipfile module from Python 3.7',
       long_description = readme,
-      author='Thomas Kluyver',
-      author_email='thomas@kluyver.me.uk',
-      url='https://gitlab.com/takluyver/zipfile36',
-      py_modules=['zipfile36'],
+      author='Markus Scheidgen, Thomas Kluyver',
+      author_email='markus.scheidgen@gmail.com',
+      url='https://github.com/markus1978/zipfile37',
+      py_modules=['zipfile37'],
       classifiers=[
           'License :: OSI Approved :: Python Software Foundation License',
           'Programming Language :: Python :: 3',
